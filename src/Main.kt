9ae610a8917eaf8af6cbf40ae208d2fab21be78d
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-03-01 20:35:40 +0630

    Learn Int, Byte, Short, Long Data types

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    val number: Int = 22
+
+    val maxNumber: Int = Int.MAX_VALUE
+    val minNumber: Int = Int.MIN_VALUE
+
+    println("Max Value: $maxNumber")
+    println("Min Value: $minNumber")
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+
+    println("Max Value: $maxByteValue")
+    println("Min Value: $minByteValue")
+
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+
+    println("Max Value: $maxShortValue")
+    println("Min Value: $minShortValue")
+
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+
+    println("Max Value: $maxLongValue")
+    println("Min Value: $minLongValue")
+}
