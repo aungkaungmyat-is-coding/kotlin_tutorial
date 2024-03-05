14f44af5f375ad233f023220044d646d016682bd
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-03-05 21:50:53 +0630

    Learn operators

---
+++ src/Operator.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val x = 5
+    val y = 3
+    println("x + y: ${x + y}")
+    println("x - y: ${x - y}")
+    println("x * y: ${x * y}")
+    println("x / y: ${x / y}")
+    println("x % y: ${x % y}")
+
+    println("3 + 3 * 4: ${3 + 3 * 4}")
+}
