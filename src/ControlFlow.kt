93c0e1bc771bdd4a559cde53dd0d0ed0fb3764a0
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-03-08 22:46:56 +0630

    Learn ControlFlow

---
+++ src/ControlFlow.kt
@@ -0,0 +1,9 @@
+fun  main () {
+    val alarm = 8
+    val text = when {
+        alarm == 8 || alarm == 7 -> "The time is $alarm"
+        alarm <= 10 -> "The number is in the range 1..10"
+        else -> "The time is $alarm"
+    }
+    println(text)
+}
