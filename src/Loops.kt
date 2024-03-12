b29daf85dcc740aa43d1635c9f50be0ad42dab18
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-03-12 20:03:17 +0630

    -

--- src/Loops.kt
+++ src/Loops.kt
@@ -1,36 +1,57 @@
 fun main() {
-    for (i in 1..10) {
-        println(i)
-    }
+    var number = 1
+    val lastNumber = 20
+    var evenNumberCounter = 0
 
-    for (i in 1 until 10) {
-        println(i)
-    }
-
-    for (i in 10 downTo 1) {
-        println(i)
-    }
-
-    for (i in 1..10 step 2) {
-        println(i)
-    }
-
-    whileLoopExample()
-}
-
-fun whileLoopExample() {
-    var number = 0
-//    while (number < 10) println(++number)
-//    while (number < 10) println(number++)
-//    while (number < 10) {
-//        println("Number: $number")
-//        number++
-//    }
-    while (number < 10) {
-        if (number == 7) {
+    while (number <= lastNumber) {
+        number++
+        if (!isEvenNumber(number)) {
             continue
         }
+        evenNumberCounter++
         println(number)
-        number++
+    }
+    println("Total number of even numbers: $evenNumberCounter")
+//    for (i in 1..10) {
+//        println(i)
+//    }
+//
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+//
+//    for (i in 10 downTo 1) {
+//        println(i)
+//    }
+//
+//    for (i in 1..10 step 2) {
+//        println(i)
+//    }
+//
+//    whileLoopExample()
+}
+
+//fun whileLoopExample() {
+//    var number = 0
+////    while (number < 10) println(++number)
+////    while (number < 10) println(number++)
+////    while (number < 10) {
+////        println("Number: $number")
+////        number++
+////    }
+//    while (number < 10) {
+//        if (number == 7) {
+//            continue
+//        }
+//        println(number)
+//        number++
+//    }
+//}
+
+fun isEvenNumber(number: Int): Boolean {
+    if ((number % 2) == 0) {
+        return true
+    } else {
+        return false
     }
 }
